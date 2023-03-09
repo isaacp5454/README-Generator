60b5ad311016ca5bcb752339d62df3d0eb2eddad
isaacp5454 <118407259+isaacp5454@users.noreply.github.com>
2023-03-08 18:46:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((data) => {
-    writeToFile("README.md",data);
+    writeToFile("./results/README.md",data);
   })
 }
 
